e747ac010d2b1309be5c286920bc789016ecb2eb
Roheet <roheetbasu@gmail.com>
2025-12-01 20:28:03 +0545

    added transformer class

--- SRC/model.py
+++ SRC/model.py
@@ -202,3 +202,31 @@ class ProjectionLayer(nn.Module):
   def forward(self, x):
     #(batch , seq , d_model) -> (batch, seq, vocab_size)
     return self.proj(x)
+
+class Transformer(nn.Module):
+
+  def __init__(self, encoder:Encoder, decoder : Decoder, src_embed : InputEmbedding, tgt_embed : InputEmbedding,src_pos: PositionalEncoding, tgt_pos: PositionalEncoding, projection_layer: ProjectionLayer):
+    super().__init__()
+    self.encoder = encoder
+    self.decoder = decoder
+    self.src_embed = src_embed
+    self.tgt_embed = tgt_embed
+    self.src_pos = src_pos
+    self.tgt_pos = tgt_pos
+    self.projection_layer = projection_layer
+
+  def encode(self, src, src_mask):
+    #(batch, seq, d_model)
+    src = self.src_embed(src)
+    src = self.src_pos(src)
+    return self.encoder(src, src_mask)
+
+  def decode(self, encoder_output, src_mask, tgt, tgt_mask):
+    #(batch, seq, d_model)
+    tgt = self.tgt_embed(tgt)
+    tgt = self.tgt_pos(tgt)
+    return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+
+  def project(self, x):
+    # (batch, seq, vocab_size)
+    return self.projection_layer(x)
