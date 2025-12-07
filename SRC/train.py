4bad9a57624df897840cabef8432b5686a92863c
Roheet <roheetbasu@gmail.com>
2025-12-07 19:09:18 +0545

    imported required libraries for training

---
+++ SRC/train.py
@@ -0,0 +1,26 @@
+from model import build_transformer
+from dataset import TranslationDataset
+from config import get_config,get_weights_file_path,latest_weights_file_path
+
+import torchtext.datasets as datasets
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.optim.lr_scheduler import LambdaLR
+
+import warnings
+from tqdm import tqdm
+import os
+from pathlib import Path
+
+#Huggingface datasets and tokenizers
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+import torchmetrics
+from torch.utils.tensorboard import SummaryWriter
+
+
