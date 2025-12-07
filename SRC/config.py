d95b7ec340661f25dc94bb32af9a3e480d9faf29
Roheet <roheetbasu@gmail.com>
2025-12-07 18:40:22 +0545

    added get_weight_file function

--- SRC/config.py
+++ SRC/config.py
@@ -14,5 +14,16 @@ def get_config():
         "model_basename" : "tmodel_",
         "preload" : "latest",
         "tokenizer_file" : "tokenizer_{0}.json",
-        "experiment_name" : "runs/tmodel"
+        "experiment_name" : "runs/tmodel" 
     }
+    
+def get_weights_file_path(config, epoch:str):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}{epoch}.pt"
+    return str(Path('.')/model_folder/model_filename)
+
+#Find the latest weights file in the weights folder
+def latest_weights_file_path(config):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}"
+    weights_file = list(Path(model_folder).glob(model_filename))
