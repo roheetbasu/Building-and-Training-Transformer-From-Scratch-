6f4eb947b6e13f079ee9514423fbb26ebb8329b3
Roheet <roheetbasu@gmail.com>
2025-12-11 13:36:58 +0545

    added function get or build tokenizer

--- SRC/train.py
+++ SRC/train.py
@@ -26,4 +26,17 @@ from torch.utils.tensorboard import SummaryWriter
 def get_all_sentences(ds ,lang):
     for item in ds:
         yield ds["translation"][lang]
-    
+        
+def get_or_build_tokenizer(config, ds, lang):
+    tokenizer_path = Path(config['tokenizer_file'].format(lang))
+    if not Path.exists(tokenizer_path):
+        #taken from :https://huggingface.co/docs/tokenizers/quicktour
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    return tokenizer
+
